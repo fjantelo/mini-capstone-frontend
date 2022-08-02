076423f9e1b0d6a49946315b790ca61522856a38
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-08-02 14:59:44 -0400

    Adds logout component

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import SignupView from "../views/SignupView.vue";
 import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
 
 const routes = [
   {
@@ -19,6 +20,7 @@ const routes = [
   },
   { path: "/signup", name: "signup", component: SignupView },
   { path: "/login", name: "login", component: LoginView },
+  { path: "/logout", name: "logout", component: LogoutView },
 ];
 
 const router = createRouter({
